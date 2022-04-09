25ec212e467a0d2daa7f9a7a0c56f18d4373233f
AnneMTreasure <AnneMTreasure@users.noreply.github.com>
2022-04-09 21:48:49 +0200

    Update 2a_data_for_kumu.R

--- scripts/2a_data_for_kumu.R
+++ scripts/2a_data_for_kumu.R
@@ -9,16 +9,16 @@ library(googlesheets4)
 
 ###### ---------- AUTHORISATIONS ---------- ######
 # this works locally
-gs4_auth(email = "*@talarify.co.za", path = "~/stakeholder_map/.secret/GSHEET_ACCESS")
+#gs4_auth(email = "*@talarify.co.za", path = "~/stakeholder_map/.secret/GSHEET_ACCESS")
 #gs4_auth(email = "*@talarify.co.za", path = "~/stakeholder_map/sheets_service_account_key.json") # before making the json a secret
 
 # for GitHub Action (adapted from https://github.com/jdtrat/tokencodr-google-demo)
-#source("functions/func_auth_google.R")
+source("functions/func_auth_google.R")
 
 # Authenticate Google Service Account (adapted from https://github.com/jdtrat/tokencodr-google-demo)
-#auth_google(email = "*@talarify.co.za",
-#            service = "GSHEET_ACCESS",
-#            token_path = ".secret/GSHEET_ACCESS")
+auth_google(email = "*@talarify.co.za",
+            service = "GSHEET_ACCESS",
+            token_path = ".secret/GSHEET_ACCESS")
 
 # Read in Google Sheet data
 form_data <- read_sheet("https://docs.google.com/spreadsheets/d/1-2rF3VNdkXzFKPjUwMexedyVFxsamjO-8yAi7AVierY/edit?resourcekey#gid=1694945086")
