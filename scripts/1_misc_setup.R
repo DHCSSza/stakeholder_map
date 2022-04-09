b49e4212bd587bae73f3b5f50fda6cfdaaac6162
Anne Treasure <anne.m.treasure@gmail.com>
2022-04-09 19:04:08 +0200

    adding encrypted json and secret

--- scripts/1_misc_setup.R
+++ scripts/1_misc_setup.R
@@ -11,12 +11,17 @@ library(googledrive)
 # just copy the password to the secret
 tokencodr::create_env_pw("GSHEET_ACCESS")
 
+## copy the password in the console. Then, paste this to the .Renviron file:
+usethis::edit_r_environ()
+# end .Renviron with a new line, save and close
+# RESTART R
+
 # Assumes the JSON file is a Google Service Account as described here:
 # https://gargle.r-lib.org/articles/get-api-credentials.html#service-account-token
 # the encrypted json file saved in .secrets folder in GitHub repo
 tokencodr::encrypt_token(service = "GSHEET_ACCESS", # "stakeholder_map"
-                         input = "sheets_service_account_key.json", 
-                         destination = "path-to-save-encrypted-json-file")
+                         input = "stakeholder-map-gsheets-access-73ff06fae0e1.json", 
+                         destination = "~/src/sadilar/stakeholder_map/")
 
 # Locally Authenticate Google Sheets & Google Drive
 googlesheets4::gs4_auth(path = tokencodr::decrypt_token(service = "GSHEET_ACCESS",
